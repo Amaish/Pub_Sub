722fd3919cb45f60807a610d1c83f2b7e2edd664
Amaina <anthonymaina.nganga@gmail.com>
2018-02-23 12:10:57 +0300

    updates on forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,19 +3,19 @@ from wtforms.validators import DataRequired, Required
 from flask_wtf import FlaskForm
 
 class Subscribe_form(FlaskForm):
-    Topic = TextAreaField('Topic', validators=[DataRequired()])
+    Topic = TextField('Topic', validators=[DataRequired()])
     submit = SubmitField('Subscribe')
     
 
 class Publish_form(FlaskForm):
-    Input = TextAreaField('Input', validators=[DataRequired()])
+    Input = TextField('Input', validators=[DataRequired()])
     submit = SubmitField('Publish')
 
 
 class Index_form(FlaskForm):
-    Broker = TextAreaField('Broker (e.g sungura1-angani-ke-host.africastalking.com)', validators=[DataRequired()])
+    Broker = TextField('Broker (e.g sungura1-angani-ke-host.africastalking.com)', validators=[DataRequired()])
     port = DecimalField('Port (e.g 10883)', validators=[DataRequired()])
-    user = TextAreaField('user', validators=[DataRequired()])
+    user = TextField('user', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    client = TextAreaField('client(e.g Anto)', validators=[DataRequired()])
+    client = TextField('client(e.g Anto)', validators=[DataRequired()])
     submit = SubmitField('submit')
